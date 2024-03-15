8f8f36dda0ce729d6cf013360837d23ff98d2795
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2024-03-16 03:09:11 +0700

    :sparkles: install react native video player

--- src/screens/LiveStream/index.jsx
+++ src/screens/LiveStream/index.jsx
@@ -1,8 +1,9 @@
 import { useNavigation, useRoute } from "@react-navigation/native"
 import React, { useState, useEffect } from "react";
-import { Box, Image } from "native-base"
+import { Box } from "native-base"
 import { useLayoutEffect } from "react";
-import { cleanImage, formatName } from "../../utils/helpers";
+import { formatName } from "../../utils/helpers";
+import Video from 'react-native-video';
 
 const LiveStream = () => {
   const route = useRoute();
@@ -21,15 +22,19 @@ const LiveStream = () => {
   }, [profile])
 
   return (
-    <Box flex="1" bg="secondary" p="3" >
-      <Image
-        size="md"
-        borderRadius="md"
-        source={{ uri: cleanImage(profile?.image) }}
-        alt="image"
-        width="100%"
-        height="215"
+    <Box flex="1" bg="secondary">
+      <Video
+        source={{ uri: "https://hls-origin275.showroom-cdn.com/liveedge/ngrp:e6f3037825315549632017c1f23198633f271752d629adc18dbc55440be63272_all/chunklist_b363457.m3u8" }}
+        style={{
+          width: "100%",
+          height: 200
+        }}
       />
+      <Box p="3">
+        <Box bg="primary" p="3">
+
+        </Box>
+      </Box>
     </Box>
   )
 }
