8af887e6125c081e4637b94919922d6f5d21d98d
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2024-03-29 22:57:47 +0700

    :zap: refactor live stream tabs

--- src/components/molecules/LiveStreamTabs/index.jsx
+++ src/components/molecules/LiveStreamTabs/index.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Box, Pressable, useColorModeValue } from "native-base";
-import { Animated, Dimensions } from 'react-native';
+import { Box, Pressable, Text, useColorModeValue } from 'native-base';
+import { Dimensions } from 'react-native';
 import { SceneMap, TabView } from 'react-native-tab-view';
-import { Comment, Podium, Room } from "../LiveStream";
+import { Comment, Podium, Room } from '../LiveStream';
 
 const initialLayout = {
-  width: Dimensions.get('window').width
+  width: Dimensions.get('window').width,
 };
 
 const renderScene = SceneMap({
@@ -14,48 +14,43 @@ const renderScene = SceneMap({
   podium: Podium,
 });
 
-export default function LiveStreamTabs() {
+const LiveStreamTabs = () => {
   const [index, setIndex] = useState(1);
-  const [routes] = useState([{
-    key: 'room',
-    title: 'Room'
-  }, {
-    key: 'comment',
-    title: 'Comment'
-  }, {
-    key: 'podium',
-    title: 'Podium'
-  }]);
+  const routes = [
+    { key: 'room', title: 'Room' },
+    { key: 'comment', title: 'Comment' },
+    { key: 'podium', title: 'Podium' },
+  ];
 
-  const renderTabBar = props => {
-    return (
-      <Box bg="primary" borderRadius="md" flexDirection="row">
-        {props.navigationState.routes.map((route, i) => {
-          const color = index === i ? useColorModeValue('white', '#e5e5e5') : useColorModeValue('#e5e5e5', 'red');
-          const borderColor = index === i ? '#ECFAFC' : useColorModeValue('gray.500', 'gray.400');
-          return (
-            <Box key={i} borderBottomWidth="3" borderColor={borderColor} flex={1} alignItems="center" p="3" cursor="pointer">
-              <Pressable onPress={() => {
-                setIndex(i);
-              }}>
-                <Animated.Text style={{
-                  color,
-                  fontWeight: i === index ? "bold" : "normal"
-                }}>
-                  {route.title}
-                </Animated.Text>
-              </Pressable>
-            </Box>
-          )
-        })}
-      </Box>
-    )
-  };
+  const renderTabBar = ({ navigationState }) => (
+    <Box bg="primary" borderRadius="md" flexDirection="row">
+      {navigationState.routes.map((route, i) => {
+        const color = index === i ? useColorModeValue('white', '#e5e5e5') : useColorModeValue('#e5e5e5', 'red');
+        const borderColor = index === i ? '#ECFAFC' : useColorModeValue('gray.500', 'gray.400');
+        
+        return (
+          <Box key={i} borderBottomWidth="3" borderColor={borderColor} flex={1} alignItems="center" p="3" cursor="pointer">
+            <Pressable onPress={() => setIndex(i)}>
+              <Text color={color} fontWeight={i === index ? 'bold' : 'normal'}>
+                {route.title}
+              </Text>
+            </Pressable>
+          </Box>
+        );
+      })}
+    </Box>
+  );
 
-  return <TabView navigationState={{
-    index,
-    routes
-  }} renderScene={renderScene} renderTabBar={renderTabBar} onIndexChange={setIndex} initialLayout={initialLayout} style={{
-    marginTop: 6
-  }} />;
-}
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      renderTabBar={renderTabBar}
+      onIndexChange={setIndex}
+      initialLayout={initialLayout}
+      style={{ marginTop: 6 }}
+    />
+  );
+};
+
+export default LiveStreamTabs;
