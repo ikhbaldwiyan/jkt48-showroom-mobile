3167083e268db847f7393303ae7b461dc09f1e57
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2024-04-08 15:28:30 +0700

    :zap: revamp podium UI and add refresh podium

--- src/components/molecules/LiveStream/Podium/index.jsx
+++ src/components/molecules/LiveStream/Podium/index.jsx
@@ -1,61 +1,92 @@
 import { useRoute } from "@react-navigation/native";
-import { Box, Center, Divider, HStack, Image, ScrollView, Text } from "native-base";
+import {
+  Center,
+  HStack,
+  Image,
+  ScrollView,
+  Text,
+  VStack
+} from "native-base";
 import { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import { RefreshControl, StyleSheet } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import { STREAM } from "../../../../services";
+import { useRefresh } from "../../../../utils/hooks/useRefresh";
 
 export const Podium = ({ isIDNLive }) => {
   const route = useRoute();
   const { params } = route;
   const [podium, setPodium] = useState([]);
+  const { refreshing, onRefresh } = useRefresh();
+
+  async function getPodiumList() {
+    const response = await STREAM.getLivePodium(params?.item?.live_id);
+    setPodium(response?.data);
+  }
+
+  async function getIDNPodiumList() {
+    const response = await STREAM.getIDNLivePodium(params?.item?.slug);
+    setPodium(response?.data);
+  }
 
   useEffect(() => {
     try {
-      async function getPodiumList() {
-        const response = await STREAM.getLivePodium(
-          params?.item?.live_id,
-        );
-        setPodium(response?.data)
-      }
-      
-      async function getIDNPodiumList() {
-        const response = await STREAM.getIDNLivePodium(
-          params?.item?.slug,
-        );
-        setPodium(response?.data)
-      }
-      
-      isIDNLive ? getIDNPodiumList() : getPodiumList()
+      isIDNLive ? getIDNPodiumList() : getPodiumList();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }, [params.item])
+  }, [params.item]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      isIDNLive ? getIDNPodiumList() : getPodiumList();
+    }, 1000);
+
+    // Set interval to fetch data every 2 minutes
+    const interval = setInterval(() => {
+      isIDNLive ? getIDNPodiumList() : getPodiumList();
+    }, 2 * 60 * 1000); // 2 minutes in milliseconds
+
+    return () => clearInterval(interval);
+  }, [params?.item?.live_id, refreshing]);
 
   return (
-    <LinearGradient colors={['#24A2B7', '#3B82F6']} style={styles.linearGradient}>
-      <ScrollView>
+    <LinearGradient
+      colors={["#24A2B7", "#3B82F6"]}
+      style={styles.linearGradient}
+    >
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
         <Center>
-          <Text fontWeight="bold">{podium?.liveData?.users} People are watching</Text>
+          <Text fontWeight="bold">
+            {podium?.liveData?.users} People are watching
+          </Text>
         </Center>
-        <Divider mt="3" />
-        {podium?.activityLog?.watch?.map((item, idx) => (
-          <HStack alignItems="center" p="3" key={idx}>
-            <Image
-              mr="3"
-              alt={item.user.name}
-              style={{ width: 50, height: 50 }}
-              source={{ uri: item?.user?.avatar }}
-            />
-            <Text fontWeight="semibold">
-              {item.user.name}
-            </Text>
-          </HStack>
-        ))}
+        <HStack
+          space={2}
+          flexWrap="wrap"
+          alignItems="center"
+          justifyContent="center"
+        >
+          {podium?.activityLog?.watch?.map((item, idx) => (
+            <VStack my="4" key={idx} alignItems="center" width="20%">
+              <Image
+                alt={item.user.name}
+                style={{ width: 50, height: 50 }}
+                source={{ uri: item?.user?.avatar }}
+              />
+              <Text mt="2" fontSize="sm" fontWeight="semibold" isTruncated>
+                {item.user.name}
+              </Text>
+            </VStack>
+          ))}
+        </HStack>
       </ScrollView>
     </LinearGradient>
-  )
+  );
 };
 
 var styles = StyleSheet.create({
@@ -64,5 +95,5 @@ var styles = StyleSheet.create({
     padding: 12,
     borderBottomLeftRadius: 6,
     borderBottomRightRadius: 6
-  },
-})
+  }
+});
