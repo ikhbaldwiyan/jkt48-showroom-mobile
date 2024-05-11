684c2d5986c2b63e61857574a2223cd40385bfee
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2024-05-11 19:37:13 +0700

    :sparkles: implement firebase cloud message for live notif member

--- src/App.js
+++ src/App.js
@@ -3,7 +3,34 @@ import { NativeBaseProvider } from "native-base";
 import { theme } from "./config";
 import Navigation from "./components/templates/Navigation";
 
+import messaging from "@react-native-firebase/messaging";
+import { PermissionsAndroid } from "react-native";
+import { useEffect } from "react";
+
 const App = () => {
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  }
+
+  const getToken = async () => {
+    const token = await messaging().getToken();
+    console.log(token);
+  };
+
+  useEffect(() => {
+    requestUserPermission();
+    getToken();
+  }, []);
+
   return (
     <NavigationContainer>
       <NativeBaseProvider theme={theme}>
--- src/components/atoms/Loading/index.jsx
+++ src/components/atoms/Loading/index.jsx
@@ -4,7 +4,7 @@ import { Box, Spinner } from "native-base";
 const Loading = () => {
   return (
     <Box flex="1" justifyContent="center" alignItems="center">
-      <Spinner size={50} color="white" />
+      <Spinner size={50} color="primary" />
     </Box>
   );
 };
