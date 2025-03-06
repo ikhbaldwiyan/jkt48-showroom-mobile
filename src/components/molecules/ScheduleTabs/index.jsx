53d460229f51ed546b5ddf9273011bbf89be252e
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2025-03-07 00:49:00 +0700

    :zap: hide songs tab if theater is ramadhan event

--- src/components/molecules/ScheduleTabs/index.jsx
+++ src/components/molecules/ScheduleTabs/index.jsx
@@ -9,7 +9,7 @@ import { SongsSetlist } from "./components/SongsSetlist";
 import { Ticket } from "./components/Ticket";
 
 const initialLayout = {
-  width: Dimensions.get("window").width,
+  width: Dimensions.get("window").width
 };
 
 const ScheduleTabs = ({ refreshing }) => {
@@ -17,15 +17,19 @@ const ScheduleTabs = ({ refreshing }) => {
   const route = useRoute();
   const { params } = route;
 
-  const routes = [
-    { key: "member", title: "Members" },
-    { key: "songs", title: "Songs" },
-    { key: "ticket", title: "Ticket" },
-  ];
-
   const [members, setMembers] = useState([]);
   const [songs, setSongs] = useState([]);
   const [ticket, setTicket] = useState({});
+  const [isRamadahanEvent, setRamadhanEvent] = useState("");
+  
+  const routes = isRamadahanEvent ? [
+    { key: "member", title: "Members" },
+    { key: "ticket", title: "Ticket" }
+  ] : [
+    { key: "member", title: "Members" },
+    { key: "songs", title: "Songs" },
+    { key: "ticket", title: "Ticket" }
+  ];
 
   useEffect(() => {
     async function getTheaterDetail() {
@@ -33,7 +37,10 @@ const ScheduleTabs = ({ refreshing }) => {
         const response = await SCHEDULES.getScheduleDetail(params.item._id);
         setMembers(response.data.memberList);
         setSongs(response.data.setlist.songs);
-        setTicket(response.data)
+        setRamadhanEvent(
+          response?.data?.setlist.originalName === "Ramadhan Event"
+        );
+        setTicket(response.data);
       } catch (error) {
         console.log(error);
       }
@@ -44,7 +51,7 @@ const ScheduleTabs = ({ refreshing }) => {
   const renderTabs = SceneMap({
     member: () => <Members members={members} />,
     songs: () => <SongsSetlist songs={songs} />,
-    ticket: () => <Ticket ticket={ticket} />,
+    ticket: () => <Ticket ticket={ticket} />
   });
 
   const renderTabBar = ({ navigationState }) => (
