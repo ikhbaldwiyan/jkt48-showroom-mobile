356c8d4e2c6a97fceeaa88f2ad2d55e032dd39ed
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2025-03-06 23:59:49 +0700

    :zap: adjust get lastest version from firestore config

--- src/components/templates/UpdateApp/index.jsx
+++ src/components/templates/UpdateApp/index.jsx
@@ -1,42 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { Linking } from "react-native";
-import { Button, Text, Center, Box, Modal } from "native-base";
-import { PLAY_STORE_URL } from "@env";
+import { Button, Text, Center, Box, Modal, Spinner } from "native-base";
+import firestore from "@react-native-firebase/firestore";
+import DeviceInfo from "react-native-device-info";
+import { firebase } from "@react-native-firebase/messaging";
 import { LogoNormal } from "../../../assets/icon";
 import trackAnalytics from "../../../utils/trackAnalytics";
 import useUser from "../../../utils/hooks/useUser";
-import { getCurrentVersion } from "../../../services/versions";
-import DeviceInfo from "react-native-device-info";
-import { firebase } from "@react-native-firebase/messaging";
+import { PLAY_STORE_URL } from "@env";
 
 const UpdateApp = () => {
   const { profile } = useUser();
   const [updateApp, setUpdateApp] = useState(false);
   const [latestVersion, setLatestVersion] = useState("");
 
-  const getVersionAndroid = async () => {
-    try {
-      const response = await getCurrentVersion();
-      setLatestVersion(response.data.version);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   useEffect(() => {
-    getVersionAndroid();
-    const currentVersion = DeviceInfo.getVersion();
+    const fetchLatestVersion = async () => {
+      try {
+        const doc = await firestore().collection("app_config").doc("env").get();
 
-    if (latestVersion > currentVersion) {
-      setUpdateApp(true);
-      firebase.messaging().subscribeToTopic("update-reminder");
-    }
-  }, [latestVersion]);
+        if (doc.exists) {
+          const data = doc.data();
+          setLatestVersion(data.APP_VERSION);
+
+          const currentVersion = DeviceInfo.getVersion();
+          if (data.APP_VERSION > currentVersion) {
+            setUpdateApp(true);
+            firebase.messaging().subscribeToTopic("update-reminder");
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching version:", error);
+      }
+    };
+
+    fetchLatestVersion();
+  }, []);
 
   const handleUpdateApp = () => {
     trackAnalytics("update_apk", {
       username: profile?.name,
-      version: latestVersion
+      version: latestVersion,
     });
     Linking.openURL(PLAY_STORE_URL);
     setUpdateApp(false);
@@ -45,7 +49,7 @@ const UpdateApp = () => {
   const closeUpdateApp = () => {
     trackAnalytics("close_update_apk", {
       username: profile?.name,
-      version: latestVersion
+      version: latestVersion,
     });
     setUpdateApp(false);
   };
@@ -59,13 +63,13 @@ const UpdateApp = () => {
     >
       <Box bg="secondary" p={5} rounded="xl" shadow={3} width="80%">
         <Text fontWeight="bold" textAlign="center" fontSize="18" mb={4}>
-          Versi Baru Tersedia Wots
+          Versi Baru Tersedia!
         </Text>
         <Center>
           <LogoNormal width={100} height={100} />
         </Center>
         <Text mt="4" textAlign="center" mb={2}>
-          Jangan lupa update APK ke versi{" "}
+          Update APK ke versi{" "}
           <Text color="primary" fontWeight="bold">
             {latestVersion}
           </Text>{" "}
