ef58998c599df2b24a2d1fefa3754e767ef41426
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2025-03-19 22:13:37 +0700

    :art: show checklist icon filter and adjust responsive

--- src/screens/LeaderboardUser/index.jsx
+++ src/screens/LeaderboardUser/index.jsx
@@ -1,4 +1,9 @@
-import React, { useState, useCallback, useEffect, useLayoutEffect } from 'react';
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+} from "react";
 import {
   Box,
   FlatList,
@@ -7,19 +12,19 @@ import {
   Text,
   Select,
   Avatar,
-  Heading,
   Badge,
   Spinner,
-} from 'native-base';
-import { getLeaderboardUser } from '../../services/leaderboard';
-import moment from 'moment';
+  CheckCircleIcon,
+} from "native-base";
+import { getLeaderboardUser } from "../../services/leaderboard";
+import moment from "moment";
 import Loading from "../../components/atoms/Loading";
 
 const LeaderboardUser = ({ navigation }) => {
   const [leaderboardData, setLeaderboardData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [platform, setPlatform] = useState("");
-  const [month, setMonth] = useState(moment().format('MM-YYYY'));
+  const [month, setMonth] = useState(moment().format("MM-YYYY"));
   const [page, setPage] = useState(1);
   const [totalData, setTotalData] = useState(0);
 
@@ -35,7 +40,7 @@ const LeaderboardUser = ({ navigation }) => {
     { name: "September", short: "09" },
     { name: "October", short: "10" },
     { name: "November", short: "11" },
-    { name: "December", short: "12" }
+    { name: "December", short: "12" },
   ];
 
   const currentYear = moment().year();
@@ -43,25 +48,27 @@ const LeaderboardUser = ({ navigation }) => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitle: "Top Leaderboard User"
+      headerTitle: "Top Leaderboard User",
     });
   }, []);
 
   const fetchLeaderboardData = useCallback(async () => {
     try {
       setLoading(true);
-      const { data: { data } } = await getLeaderboardUser({
+      const {
+        data: { data },
+      } = await getLeaderboardUser({
         page,
         month,
         platform,
-        filterBy: month !== "" ? "month" : ""
+        filterBy: month !== "" ? "month" : "",
       });
       console.log(data);
       setLeaderboardData(data.data);
       setTotalData(data.pagination.totalData);
       setPage(data.pagination.currentPage);
     } catch (error) {
-      console.error('Error fetching leaderboard:', error);
+      console.error("Error fetching leaderboard:", error);
     } finally {
       setLoading(false);
     }
@@ -77,13 +84,21 @@ const LeaderboardUser = ({ navigation }) => {
   };
 
   const handleMonthChange = (value) => {
-    console.log(value);
     setMonth(value);
     setPage(1);
   };
 
   const ListHeader = () => (
-    <Box bg="cyan.600" p={3} borderBottomWidth={1} borderBottomColor="blueLight" rounded="lg" borderBottomLeftRadius={0} borderBottomRightRadius={0} mb={2}>
+    <Box
+      p={3}
+      mb={2}
+      bg="cyan.600"
+      borderBottomWidth={1}
+      borderBottomColor="blueLight"
+      rounded="lg"
+      borderBottomLeftRadius={0}
+      borderBottomRightRadius={0}
+    >
       <HStack space={4} alignItems="center">
         <Box w={8} alignItems="center">
           <Text fontSize="sm" color="white" fontWeight="bold">
@@ -103,7 +118,7 @@ const LeaderboardUser = ({ navigation }) => {
           </Text>
         </Box>
 
-        <Box w={16} alignItems="center">
+        <Box alignItems="center">
           <Text fontSize="sm" color="white" fontWeight="bold">
             Total Watch
           </Text>
@@ -114,43 +129,43 @@ const LeaderboardUser = ({ navigation }) => {
 
   const renderItem = ({ item, index }) => {
     return (
-      <Box
-        mb={2}
-        p={3}
-        shadow={1}
-        rounded="lg"
-        key={item.rank}
-        bg="cyan.700"
-      >
+      <Box mb={2} p={3} shadow={1} rounded="lg" key={item.rank} bg="cyan.700">
         <HStack space={4} alignItems="center">
           <Box w={8} alignItems="center">
             <Text
               fontSize="lg"
               fontWeight="bold"
-              color={index < 3 ? 'amber.400' : 'gray.200'}
+              color={index < 3 ? "amber.400" : "gray.200"}
             >
               {item.rank}
             </Text>
           </Box>
 
-          <Avatar
-            size="md"
-            source={{ uri: item.avatar }}
-          />
+          <Avatar size="md" source={{ uri: item.avatar }} />
 
           <VStack flex={1}>
-            <Text color="blueLight" fontSize="md" fontWeight="semibold">
-              {item.user_id}
+            <Text
+              color="blueLight"
+              fontSize="md"
+              fontWeight="semibold"
+              isTruncated
+            >
+              {item.user_id.length > 13
+                ? `${item.user_id.slice(0, 13)}...`
+                : item?.user_id}
             </Text>
           </VStack>
 
           <HStack space={2} alignItems="center">
             <VStack alignItems="center">
-              <Badge colorScheme="info" rounded="full" width={platform ? 10 : 'auto'}>
+              <Badge colorScheme="info" rounded="full" width={"auto"}>
                 <Text fontWeight="bold" color="primary">
-                  {platform === 'Showroom' ? item.watchShowroomMember :
-                   platform === 'IDN' ? item.watchLiveIDN :
-                   item.watchShowroomMember + item.watchLiveIDN}x
+                  {platform === "Showroom"
+                    ? item.watchShowroomMember
+                    : platform === "IDN"
+                    ? item.watchLiveIDN
+                    : item.watchShowroomMember + item.watchLiveIDN}
+                  x
                 </Text>
               </Badge>
             </VStack>
@@ -171,9 +186,39 @@ const LeaderboardUser = ({ navigation }) => {
             placeholder="Select Platform"
             color="white"
           >
-            <Select.Item label="All Platform" value="" />
-            <Select.Item label="Showroom" value="Showroom" />
-            <Select.Item label="IDN" value="IDN" />
+            <Select.Item
+              label="All Platform Live"
+              value=""
+              endIcon={
+                platform === "" ? (
+                  <Box mt="1">
+                    <CheckCircleIcon size="4" color="primary" />
+                  </Box>
+                ) : null
+              }
+            />
+            <Select.Item
+              label="Showroom"
+              value="Showroom"
+              endIcon={
+                platform === "Showroom" ? (
+                  <Box mt="1">
+                    <CheckCircleIcon size="4" color="primary" />
+                  </Box>
+                ) : null
+              }
+            />
+            <Select.Item
+              label="IDN Live"
+              value="IDN"
+              endIcon={
+                platform === "IDN" ? (
+                  <Box mt="1">
+                    <CheckCircleIcon size="4" color="primary" />
+                  </Box>
+                ) : null
+              }
+            />
           </Select>
 
           <Select
@@ -183,18 +228,31 @@ const LeaderboardUser = ({ navigation }) => {
             placeholder="Select Month"
             color="white"
           >
-            <Select.Item label="All Time" value="" />
             <Select.Item
-              label="This Month"
-              value={moment().format('MM-YYYY')}
+              label="All Time"
+              value=""
+              endIcon={
+                month === "" ? (
+                  <Box mt="1">
+                    <CheckCircleIcon size="4" color="primary" />
+                  </Box>
+                ) : null
+              }
             />
             {monthNames.map((m) => {
-              if (m.short > moment().format('MM')) return null;
+              if (m.short > moment().format("MM")) return null;
               return (
                 <Select.Item
                   key={`${m.short}-${currentYear}`}
                   label={`${m.name} ${currentYear}`}
                   value={`${m.short}-${currentYear}`}
+                  endIcon={
+                    month === `${m.short}-${currentYear}` ? (
+                      <Box mt="1">
+                        <CheckCircleIcon size="4" color="primary" />
+                      </Box>
+                    ) : null
+                  }
                 />
               );
             })}
@@ -203,6 +261,13 @@ const LeaderboardUser = ({ navigation }) => {
                 key={`${m.short}-${previousYear}`}
                 label={`${m.name} ${previousYear}`}
                 value={`${m.short}-${previousYear}`}
+                endIcon={
+                  month === `${m.short}-${previousYear}` ? (
+                    <Box mt="1">
+                      <CheckCircleIcon size="4" color="primary" />
+                    </Box>
+                  ) : null
+                }
               />
             ))}
           </Select>
@@ -220,13 +285,18 @@ const LeaderboardUser = ({ navigation }) => {
               onEndReachedThreshold={0.5}
               ListHeaderComponent={ListHeader}
               stickyHeaderIndices={[0]}
-              ListFooterComponent={() => (
+              ListFooterComponent={() =>
                 loading && page > 1 ? (
-                  <Box flex={1} justifyContent="center" alignItems="center" h="full">
+                  <Box
+                    flex={1}
+                    justifyContent="center"
+                    alignItems="center"
+                    h="full"
+                  >
                     <Loading size="30" />
                   </Box>
                 ) : null
-              )}
+              }
             />
           </Box>
         )}
