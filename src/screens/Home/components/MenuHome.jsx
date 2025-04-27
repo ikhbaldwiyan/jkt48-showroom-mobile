4b6ba6f70077094c463bc428e0780d31a7d94ece
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2025-04-27 23:06:08 +0700

    :bug: fix routing and add key to menu home

--- src/screens/Home/components/MenuHome.jsx
+++ src/screens/Home/components/MenuHome.jsx
@@ -39,28 +39,29 @@ const MenuHome = () => {
     {
       name: "IDN Live",
       icon: <PlayIcon size="6" color="white" />,
-      route: "IDNLives"
+      screen: "IDNLives"
     },
     {
       name: "Top Member",
       icon: <Medal size="24" color="white" />,
-      route: "LeaderboardMember"
+      screen: "LeaderboardMember"
     }
   ];
 
   return (
     <Box mb="4">
-      <Text py="2" fontSize="2xl" fontWeight="semibold">
+      <Text mb="3" fontSize="2xl" fontWeight="semibold">
         Menu
       </Text>
       <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
         <HStack space={3}>
           {menus.map((item, idx) => (
             <TouchableOpacity
+              key={idx}
               activeOpacity={0.8}
               onPress={() => navigate(item.screen)}
             >
-              <Box key={idx} p="2.5" px="3" bg="primary" borderRadius={10}>
+              <Box p="2.5" px="3" bg="primary" borderRadius={10}>
                 <VStack space={1} justifyContent="center" alignItems="center">
                   {item.icon}
                   <Box mt="2" p="0.9" px="2" bg="blueLight" borderRadius={10}>
