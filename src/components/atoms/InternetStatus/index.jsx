dba9bd6632b8db4131326a2b364c7d2e51dfcf88
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2025-06-29 17:59:42 +0700

    :bug: hotfix hide internet status on landscape

--- src/components/atoms/InternetStatus/index.jsx
+++ src/components/atoms/InternetStatus/index.jsx
@@ -3,10 +3,13 @@ import NetInfo from "@react-native-community/netinfo";
 import { Slide, Alert, Text, VStack } from "native-base";
 import { OfflineIcon } from "../../../assets/icon";
 import usePipStore from "../../../store/usePipStore";
+import { useWindowDimensions } from "react-native";
 
 const InternetStatusInfo = () => {
   const { isPipMode } = usePipStore();
   const [isConnected, setIsConnected] = useState(true);
+  const { width, height } = useWindowDimensions();
+  const isLandscape = width > height;
 
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener((state) => {
@@ -18,34 +21,33 @@ const InternetStatusInfo = () => {
     };
   }, []);
 
+  if (isPipMode || isLandscape) return null;
+
   return (
-    !isPipMode && (
-      <Slide in={!isConnected} placement="top" mt="280" mx="6">
-        <Alert
-          bg="blueGray.800"
-          borderColor="white"
-          borderWidth={2}
-          status="error"
-          borderRadius="xl"
-          safeAreaTop
+    <Slide in={!isConnected} placement="top" mt="280" mx="6">
+      <Alert
+        bg="blueGray.800"
+        borderColor="white"
+        borderWidth={2}
+        status="error"
+        borderRadius="xl"
+        safeAreaTop
+      >
+        <VStack
+          alignItems="center"
+          justifyContent="center"
+          space={3}
+          px="3"
+          py="2"
+          textAlign="center"
         >
-          <VStack
-            alignItems="center"
-            justifyContent="center"
-            space={3}
-            px="3"
-            py="2"
-            textAlign="center"
-          >
-            <OfflineIcon size={55} />
-            <Text fontSize="md" textAlign="center" fontWeight="medium">
-              Tidak ada koneksi internet, Silakan periksa sambungan internet
-              kamu.
-            </Text>
-          </VStack>
-        </Alert>
-      </Slide>
-    )
+          <OfflineIcon size={55} />
+          <Text fontSize="md" textAlign="center" fontWeight="medium">
+            Tidak ada koneksi internet, Silakan periksa sambungan internet kamu.
+          </Text>
+        </VStack>
+      </Alert>
+    </Slide>
   );
 };
 
