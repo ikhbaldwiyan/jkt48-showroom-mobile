62c3f71bb04217a8cbb523507c99f6a5ea2b2730
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2025-10-11 23:42:35 +0700

    :zap: add watching user on history detail

--- src/screens/HistoryLiveDetail/index.jsx
+++ src/screens/HistoryLiveDetail/index.jsx
@@ -28,6 +28,7 @@ import Layout from "../../components/templates/Layout";
 import Screenshot from "./components/Screenshot";
 import HistoryLiveTabs from "../../components/molecules/HistoryLiveTabs";
 import WebView from "react-native-webview";
+import { WatchingUser } from "../../components/molecules/HistoryLiveTabs/components";
 
 const HistoryLiveDetail = ({ route }) => {
   const navigation = useNavigation();
@@ -53,143 +54,151 @@ const HistoryLiveDetail = ({ route }) => {
       <Loading />
     </Box>
   ) : (
-    <Layout>
-      <HStack justifyContent="space-between" alignItems="center">
-        <Box>
-          <Text fontSize="xl" fontWeight="semibold">
-            {data?.room_info?.nickname}
-          </Text>
-          <Text color="gray.400">{data?.room_info?.fullname}</Text>
-        </Box>
-        {isShowroom ? (
-          <Image
-            size="md"
-            alt="showroom"
-            source={{
-              uri: "https://play-lh.googleusercontent.com/gf9vm7y3PgUGzGrt8pqJNtqb6x0AGzojrKlfntGvPyGQSjmPwAls35zZ-CXj_jryA8k",
-            }}
-            width="42"
-            height="42"
-            rounded="md"
-          />
-        ) : (
-          <IDNLiveIcon />
-        )}
-      </HStack>
-
-      {replay !== undefined && isSuccess && replay.length > 1 ? (
-        <Box mt="3" borderRadius={6} overflow="hidden">
-          <WebView
-            style={{
-              width: "100%",
-              height: isShowroom ? 188 : 412,
-            }}
-            source={{
-              uri:
-                replay !== undefined && isSuccess
-                  ? `https://www.youtube.com/embed/${replay}`
-                  : null,
-            }}
-            allowsFullscreenVideo
-          />
-        </Box>
-      ) : (
-        <Screenshot
-          thumbnail={data?.room_info?.img}
-          images={images}
-          folder={folder}
-          isShowroom={isShowroom}
-        />
-      )}
-
-      <Box my="3">
-        <Text fontSize="xl" fontWeight="semibold">
-          Detail
-        </Text>
-
-        <HStack flexWrap="wrap" mt="3">
-          <VStack w="50%" mb="4" pr="2" space={1}>
-            <HStack space={1.5} alignItems="center">
-              <Calendar color="#A3A3A3" />
-              <Text color="gray.400" fontSize={13}>
-                Tanggal
-              </Text>
-            </HStack>
-            <Text>
-              {moment(data?.live_info?.date?.start).format("dddd, DD MMM YYYY")}
+    <>
+      <Layout>
+        <HStack justifyContent="space-between" alignItems="center">
+          <Box>
+            <Text fontSize="xl" fontWeight="semibold">
+              {data?.room_info?.nickname}
             </Text>
-          </VStack>
-
-          <VStack w="50%" mb="4" pl="2" space={1}>
-            <HStack space={1.5} alignItems="center">
-              <TimesIcon color="#A3A3A3" size={15} />
-              <Text color="gray.400" fontSize={13}>
-                Durasi Live
-              </Text>
-            </HStack>
-            <Text>{getLiveDurationMinutes(data?.live_info?.duration)}</Text>
-          </VStack>
-
-          <VStack w="50%" mb="4" pr="2" space={1}>
-            <HStack space={1.5} alignItems="center">
-              <StartIcon size={20} />
-              <Text color="gray.400" fontSize={13}>
-                Mulai
-              </Text>
-            </HStack>
-            <Text>{formatLongDate(data?.live_info?.date?.start, true)}</Text>
-          </VStack>
-
-          <VStack w="50%" mb="4" pl="2" space={1}>
-            <HStack space={1.5} alignItems="center">
-              <StopIcon size={20} />
-              <Text color="gray.400" fontSize={13}>
-                Selesai
-              </Text>
-            </HStack>
-            <Text>{formatLongDate(data?.live_info?.date?.end, true)}</Text>
-          </VStack>
-
-          <VStack w="50%" mb="4" pr="2" space={1}>
-            <HStack space={1.5} alignItems="center">
-              <LiveIcon size="16" color="#A3A3A3" />
-              <Text color="gray.400" fontSize={13}>
-                Judul Live
-              </Text>
-            </HStack>
-            <Text>{data?.idn?.title ?? "-"}</Text>
-          </VStack>
-          <VStack w="50%" mb="4" pl="2" space={1}>
-            <HStack space={1.5} alignItems="center">
-              <EyeIcon color="#A3A3A3" size="17" />
-              <Text color="gray.400" fontSize={13}>
-                Penonton
-              </Text>
-            </HStack>
-            <Text>{formatViews(data?.live_info?.viewers?.num)}</Text>
-          </VStack>
-
-          <VStack w="50%" mb="4" pr="2" space={1}>
-            <HStack space={1.5} alignItems="center">
-              <ChatIcon color="#A3A3A3" size="18" />
-              <Text color="gray.400" fontSize={13}>
-                Komentar
-              </Text>
-            </HStack>
-            <Text>{formatViews(data?.live_info?.comments?.num)} chat</Text>
-          </VStack>
-          <VStack w="50%" mb="4" pl="2" space={1}>
-            <HStack space={1.5} alignItems="center">
-              <GiftFill size="16" color="#A3A3A3" />
-              <Text color="gray.400" fontSize={13}>
-                Gifts
-              </Text>
-            </HStack>
-            <Text>{formatViews(data?.total_gifts)}</Text>
-          </VStack>
+            <Text color="gray.400">{data?.room_info?.fullname}</Text>
+          </Box>
+          {isShowroom ? (
+            <Image
+              size="md"
+              alt="showroom"
+              source={{
+                uri: "https://play-lh.googleusercontent.com/gf9vm7y3PgUGzGrt8pqJNtqb6x0AGzojrKlfntGvPyGQSjmPwAls35zZ-CXj_jryA8k",
+              }}
+              width="42"
+              height="42"
+              rounded="md"
+            />
+          ) : (
+            <IDNLiveIcon />
+          )}
         </HStack>
-      </Box>
-    </Layout>
+
+        {replay !== undefined && isSuccess && replay.length > 1 ? (
+          <Box mt="3" borderRadius={6} overflow="hidden">
+            <WebView
+              style={{
+                width: "100%",
+                height: isShowroom ? 188 : 412,
+              }}
+              source={{
+                uri:
+                  replay !== undefined && isSuccess
+                    ? `https://www.youtube.com/embed/${replay}`
+                    : null,
+              }}
+              allowsFullscreenVideo
+            />
+          </Box>
+        ) : (
+          <Screenshot
+            thumbnail={data?.room_info?.img}
+            images={images}
+            folder={folder}
+            isShowroom={isShowroom}
+          />
+        )}
+
+        <Box my="3">
+          <Text fontSize="xl" fontWeight="semibold">
+            Detail
+          </Text>
+
+          <HStack flexWrap="wrap" mt="3">
+            <VStack w="50%" mb="4" pr="2" space={1}>
+              <HStack space={1.5} alignItems="center">
+                <Calendar color="#A3A3A3" />
+                <Text color="gray.400" fontSize={13}>
+                  Tanggal
+                </Text>
+              </HStack>
+              <Text>
+                {moment(data?.live_info?.date?.start).format(
+                  "dddd, DD MMM YYYY"
+                )}
+              </Text>
+            </VStack>
+
+            <VStack w="50%" mb="4" pl="2" space={1}>
+              <HStack space={1.5} alignItems="center">
+                <TimesIcon color="#A3A3A3" size={15} />
+                <Text color="gray.400" fontSize={13}>
+                  Durasi Live
+                </Text>
+              </HStack>
+              <Text>{getLiveDurationMinutes(data?.live_info?.duration)}</Text>
+            </VStack>
+
+            <VStack w="50%" mb="4" pr="2" space={1}>
+              <HStack space={1.5} alignItems="center">
+                <StartIcon size={20} />
+                <Text color="gray.400" fontSize={13}>
+                  Mulai
+                </Text>
+              </HStack>
+              <Text>{formatLongDate(data?.live_info?.date?.start, true)}</Text>
+            </VStack>
+
+            <VStack w="50%" mb="4" pl="2" space={1}>
+              <HStack space={1.5} alignItems="center">
+                <StopIcon size={20} />
+                <Text color="gray.400" fontSize={13}>
+                  Selesai
+                </Text>
+              </HStack>
+              <Text>{formatLongDate(data?.live_info?.date?.end, true)}</Text>
+            </VStack>
+
+            <VStack w="50%" mb="4" pr="2" space={1}>
+              <HStack space={1.5} alignItems="center">
+                <LiveIcon size="16" color="#A3A3A3" />
+                <Text color="gray.400" fontSize={13}>
+                  Judul Live
+                </Text>
+              </HStack>
+              <Text>{data?.idn?.title ?? "-"}</Text>
+            </VStack>
+            <VStack w="50%" mb="4" pl="2" space={1}>
+              <HStack space={1.5} alignItems="center">
+                <EyeIcon color="#A3A3A3" size="17" />
+                <Text color="gray.400" fontSize={13}>
+                  Penonton
+                </Text>
+              </HStack>
+              <Text>{formatViews(data?.live_info?.viewers?.num)}</Text>
+            </VStack>
+
+            <VStack w="50%" mb="4" pr="2" space={1}>
+              <HStack space={1.5} alignItems="center">
+                <ChatIcon color="#A3A3A3" size="18" />
+                <Text color="gray.400" fontSize={13}>
+                  Komentar
+                </Text>
+              </HStack>
+              <Text>{formatViews(data?.live_info?.comments?.num)} chat</Text>
+            </VStack>
+            <VStack w="50%" mb="4" pl="2" space={1}>
+              <HStack space={1.5} alignItems="center">
+                <GiftFill size="16" color="#A3A3A3" />
+                <Text color="gray.400" fontSize={13}>
+                  Gifts
+                </Text>
+              </HStack>
+              <Text>{formatViews(data?.total_gifts)}</Text>
+            </VStack>
+          </HStack>
+          <WatchingUser
+            platform={isShowroom ? "showroom" : "idn"}
+            liveId={liveSlug}
+          />
+        </Box>
+      </Layout>
+    </>
   );
 };
 
